65a32acc500104817dc256ab4eb13f6184540f64
Bryn <bryndrury@icloud.com>
2024-11-07 10:42:45 +0000

    Added orange to butterfly

--- pixel_art.py
+++ pixel_art.py
@@ -30,7 +30,8 @@ def define_butterfly():
 
     # define some colours
     black = [0, 0, 0]
-    blue = [0.2, 0.4, 0.8]
+    red = [0.8, 0.2, 0.2]
+    orange = [1.0, 0.6, 0.0]
 
     # specify which pixels are which colour
     c = 8
@@ -58,15 +59,15 @@ def define_butterfly():
         image_mat[11, i*1+c] = black
         
         # fill in the centre line by line
-        image_mat[3, [i*j+c for j in [4, 5]]] = blue
-        image_mat[4, [i*j+c for j in range(2,7)]] = blue
-        image_mat[5:7, [i*j+c for j in range(1,7)]] = blue
-        image_mat[7, [i*j+c for j in range(2,5)]] = blue
-        image_mat[8, i*1+c] = blue
-        image_mat[9, [i*j+c for j in range(1,5)]] = blue
-        image_mat[10, [i*j+c for j in range(1,6)]] = blue
-        image_mat[11, [i*j+c for j in range(2,6)]] = blue
-        image_mat[12, [i*j+c for j in range(3,5)]] = blue
+        image_mat[3, [i*j+c for j in [4, 5]]] = red
+        image_mat[4, [i*j+c for j in range(2,7)]] = red
+        image_mat[5:7, [i*j+c for j in range(1,7)]] = red
+        image_mat[7, [i*j+c for j in range(2,5)]] = red
+        image_mat[8, i*1+c] = orange
+        image_mat[9, [i*j+c for j in range(1,5)]] = red
+        image_mat[10, [i*j+c for j in range(1,6)]] = red
+        image_mat[11, [i*j+c for j in range(2,6)]] = orange
+        image_mat[12, [i*j+c for j in range(3,5)]] = red
     
     return image_mat
 
